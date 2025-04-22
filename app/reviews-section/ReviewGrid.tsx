d060130fbf8caca2640b1bddf00a169a006384f7
vikramjeet singh <vikramjeetsinghs341@gmail.com>
2025-04-22 15:37:52 +0100

    added

--- app/reviews-section/ReviewGrid.tsx
+++ app/reviews-section/ReviewGrid.tsx
@@ -14,7 +14,7 @@ const Reviews = () => {
         className={`relative mb-16 flex w-full flex-col items-center justify-center gap-10  text-[#e4ded7] sm:items-center lg:max-w-[1440px]`}
       >
         <AnimatedWords2
-          title={"Testimonials"}
+          title={"Certifications"}
           style={`flex max-w-[500px] flex-col items-start text-left pr-5 ${monaSans.className} font-extrabold uppercase leading-[0.9em] text-[#e4ded7] sm:max-w-full sm:flex-row sm:items-center sm:justify-center sm:text-center lg:text-center text-[clamp(70px,14vw,155.04px)]`}
         />
         <AnimatedBody
